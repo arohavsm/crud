3d80281de03d48706c7adfd8276e55fd1b293926
pamuleti <pamuleti@aroha.co.in-at-613112965612>
2019-08-26 17:26:55 +0530

    Comments API

--- server/api/comments/comments.restRouter.js
+++ server/api/comments/comments.restRouter.js
@@ -3,11 +3,26 @@ import {
   getAll, getCommentById, add, update, remove
  } from './comments.controller'
 
+ import { getHeader } from '../../auth/auth.middleware'
+
 export const commentsRouter = express.Router()
+
+commentsRouter.use((req,res,next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'POST,GET,PUT,DELETE,OPTIONS')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, x-requested-with, x-requested-by ,Content-Type, Authorization, Accept, Access-Control-Al, low-methods, X-Custom-Header'
+  )
+  res.header('X-Frame-Options', 'deny')
+  res.header('C-Content-Type-Optoins', 'nosniff')
+
+  next()
+})
 
 commentsRouter.route('/')
   .get(getAll)
-  .post(add)
+  .post(getHeader, add)
 
 commentsRouter.route('/:id')
  .get(getCommentById)
--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 import express from 'express'
+import cors from 'cors'
 
 import { connect } from './db/index'
 import { authRestRouter } from './auth/index'
 import { restRouter } from './api/restRouter'
 import { globalMiddleware } from './utils/globalMiddleware'
+import { getHeader } from './auth/auth.middleware'
 
 connect()
 
@@ -20,16 +22,29 @@ app.use((req,res,next) => {
   res.header('Access-Control-Allow-Methods', 'POST,GET,PUT,DELETE,OPTIONS')
   res.header(
     'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Authorization, Accept, Access-Control-Al, low-methods'
+    'Origin, X-Requested-With, x-requested-with, x-requested-by ,Content-Type, Authorization, Accept, Access-Control-Al, low-methods, X-Custom-Header'
   )
   res.header('X-Frame-Options', 'deny')
   res.header('C-Content-Type-Optoins', 'nosniff')
 
   next()
+
+})
+
+app.get('/', getHeader, (req, res) => {
+  res.send({
+    message: "OK"
+  })
+})
+
+app.post('/', getHeader, (req, res) => {
+  res.send(req.body)
 })
 
 app.use('/auth', authRestRouter)
 app.use('/api', restRouter)
+
+// app.use(cors({credentials: true, origin: true})
 
 app.listen(8090, function(){
   console.log('server is running on port 8090')
